d872fbc0d5b2b8593951eb0966c61a21f7649df9
EC2 Default User <ec2-user@ip-172-31-22-63.us-east-2.compute.internal>
2020-05-12 15:59:40 +0000

    API-Cache

--- FIN - API/createAPI.py
+++ FIN - API/createAPI.py
@@ -23,11 +23,23 @@ response3 = client.create_resource(
     pathPart = "transaction",
 )
 
+cachemethod = client.create_resource(
+    restApiId = response['id'],
+    parentId = response2['items'][0]['id'],
+    pathPart = "cache",
+)
+
 func = lam.get_function(
     FunctionName='transactions'
 )
 
+funccache = lam.get_function(
+    FunctionName='cache'
+)
+
 arnl = func['Configuration']['FunctionArn']
+
+arnc = funccache['Configuration']['FunctionArn']
 
 response5 = client.put_method(
     restApiId = response['id'],
@@ -45,6 +57,26 @@ response4 = client.put_integration(
     uri='arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/'+arnl+'/invocations'
 )
 
+
+#################################### CACHE METHOD ##############################
+response6 = client.put_method(
+    restApiId = response['id'],
+    resourceId = cachemethod['id'],
+    httpMethod='GET',
+    authorizationType='NONE'
+)
+
+response7 = client.put_integration(
+    restApiId = response['id'],
+    resourceId = cachemethod['id'],
+    httpMethod = 'GET',
+    type='AWS_PROXY',
+    integrationHttpMethod='POST',
+    uri='arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/'+arnc+'/invocations'
+)
+
+###################################################################################
+
 deploy = client.create_deployment(
     restApiId = response['id'],
     stageName ='prod'
@@ -54,7 +86,15 @@ print('https://'+response['id']+'.execute-api.us-east-2.amazonaws.com/prod')
 
 permi = lam.add_permission(
     FunctionName='transactions',
-    StatementId='apigateway-finch-aws12',
+    StatementId='apigateway-finch-aws4',
+    Action='lambda:InvokeFunction',
+    Principal='apigateway.amazonaws.com',
+    SourceArn='arn:aws:execute-api:us-east-2:591336854477:'+response['id']+'/prod/*'
+)
+
+permi2 = lam.add_permission(
+    FunctionName='cache',
+    StatementId='apigateway-finch-aws3',
     Action='lambda:InvokeFunction',
     Principal='apigateway.amazonaws.com',
     SourceArn='arn:aws:execute-api:us-east-2:591336854477:'+response['id']+'/prod/*'
