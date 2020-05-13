d32840dc785eecf1a0482757c62e18b7ce40f9c5
EC2 Default User <ec2-user@ip-172-31-22-63.us-east-2.compute.internal>
2020-05-13 23:30:53 +0000

    Cache random fixed

--- FIN - Verificaciones/src/cache.py
+++ FIN - Verificaciones/src/cache.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+import random
+from random import randint
 from boto3.dynamodb.conditions import Key, Attr
 
 
@@ -12,13 +14,46 @@ def handler(event, context):
         KeyConditionExpression=Key('pk').eq(sender)
     )
     
-    body = {
-        "Client code" : sender,
-        "Name": response["Items"][0]["name"],
-        "Company" : response["Items"][0]["sk"],
-        "CI": float(response["Items"][0]["CI"]),
-        "Money made": float(response["Items"][0]["m-made"])
-    }
+    info = response["Items"]
+    
+    if len(response["Items"]) == 0:
+        first = ("Camila", "Emmi", "Abigail", "Martin", "Mauricio", "Sara", "Enrique", "Mariana", "Joaquin", "Andres", "Ramiro", "Adrian", "Sergio", "Vivian", "Valeria", "Erica", "Raisa", "Daniela", "Alejandra", "Jazmin", "Liz")
+        second = ("Fernandez", "Lopez", "Martinez", "Sanchez", "Perez", "Gomez", "Mercury", "Jackson", "Torrez", "Alvarez", "Ruiz", "Ramirez", "Benitez", "Acosta", "Medina", "Arze", "Rojas", "Molina", "Castro")
+        firrst = random.choice(first)
+        seccond = random.choice(second)
+        name = (firrst + " " + seccond )
+        
+        company ="company-" + str(randint(1,101))
+        CI = randint(1000000, 9999999)
+        mmade = randint(0, 99999999999999) 
+        
+        response = app.put_item(
+            Item={
+                "pk": sender,
+                "sk": company,
+                "CI": CI,
+                "m-made": mmade,
+                "name": name,
+            }
+        )
+        
+            
+        body = {
+            "Client code" : sender,
+            "Name": name,
+            "Company" : company,
+            "CI": CI,
+            "Money made": mmade
+        }
+        
+    else:
+        body = {
+            "Client code" : sender,
+            "Name": info[0]["name"],
+            "Company" : info[0]["sk"],
+            "CI": float(info[0]["CI"]),
+            "Money made": float(info[0]["m-made"])
+        }
     
     return {
         "statusCode" : 200,
